122683db5f46d9a2d0765a184fc418c29b2d9b7e
Vinicius Moreira <viniciuslm@gmail.com>
2022-02-18 06:47:48 -0300

    first commit

---
+++ lib/reports_generator.ex
@@ -0,0 +1,15 @@
+defmodule ReportsGenerator do
+  alias ReportsGenerator.Parser
+
+  def build(filename) do
+    filename
+    |> Parser.parse_file()
+    |> Enum.reduce(report_acc(), fn line, report -> sum_values(line, report) end)
+  end
+
+  def fetch_higer_cost(report), do: Enum.max_by(report, fn { _key, value} -> value end)
+
+  defp sum_values([id, _food, price ], report), do: Map.put(report, id, report[id] + price)
+
+  defp report_acc, do: Enum.into(1..30, %{}, &{Integer.to_string(&1), 0})
+end
